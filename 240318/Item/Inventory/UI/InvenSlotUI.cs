c7ba0baa53335c5d28d400714b4d21cfa8320778
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-03-18 14:18:39 +0900

    Add files via upload

---
+++ 240318/Item/Inventory/UI/InvenSlotUI.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InvenSlotUI : SlotUI_Base, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerClickHandler
+{
+    /// <summary>
+    /// 드래그 시작을 알리는 델리게이트 (uint : 드래그 시작 슬롯의 인덱스)
+    /// </summary>
+    public Action<uint> onDragBegin;
+
+    /// <summary>
+    /// 드래그 종료를 알리는 델리게이트 (uint : 드래그가 끝난 슬롯의 인덱스, bool : 슬롯에서 드래그가 끝났으면 true)
+    /// </summary>
+    public Action<uint, bool> onDragEnd;
+
+    /// <summary>
+    /// 마우스 클릭을 알리는 델리게이트 (uint : 클릭이 된 슬롯의 인덱스)
+    /// </summary>
+    public Action<uint> onClick;
+
+    /// <summary>
+    /// 장비 여부 표시용 텍스트
+    /// </summary>
+    TextMeshProUGUI equipText;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Transform child = transform.GetChild(2);
+        equipText = child.GetComponent<TextMeshProUGUI>();
+    }
+
+    protected override void OnRefresh()
+    {
+        // 화면 갱신할 때, 장비 여부에 따라 장비 글자 색 변경
+        if (InvenSlot.IsEquipped)
+        {
+            equipText.color = Color.red; // 장비했을 때는 빨간색
+        }
+        else
+        {
+            equipText.color = Color.clear; // 장비하지 않았을 때는 투명
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log($"드래그 시작 : [{Index}]번 슬롯");
+        onDragBegin?.Invoke(Index);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // OnBeginDrag와 OnEndDrag용
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GameObject obj = eventData.pointerCurrentRaycast.gameObject; // UI 대상 레이캐스트
+        if (obj != null)
+        {
+            // 마우스 위치에 어떤 UI가 있다.
+            InvenSlotUI endSlot = obj.GetComponent<InvenSlotUI>();
+            if (endSlot != null)
+            {
+                // 슬롯이다.
+                //Debug.Log($"드래그 종료 : [{endSlot.Index}]번 슬롯");
+                onDragEnd?.Invoke(endSlot.Index, true);
+            }
+            else
+            {
+                // 슬롯이 아니다.
+                Debug.Log($"[{obj.gameObject.name}]은 슬롯이 아닙니다.");
+                onDragEnd?.Invoke(Index, false);
+            }
+        }
+        else
+        {
+            // 마우스 위치에 어떤 UI도 없다.
+            Debug.Log("어떤 UI도 없다.");
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        onClick?.Invoke(Index);
+    }
+}
---
+++ 240318/Item/Inventory/UI/InventoryUI.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    /// <summary>
+    /// 이 UI가 보여줄 인벤토리
+    /// </summary>
+    Inventory inven;
+
+    /// <summary>
+    /// 인벤토리에 있는 slot UI들
+    /// </summary>
+    InvenSlotUI[] slotUIs;
+
+    TempSlotUI tempSLotUI;
+
+    private void Awake()
+    {
+        Transform child = transform.GetChild(0);
+        slotUIs = child.GetComponentsInChildren<InvenSlotUI>();
+
+        tempSLotUI = GetComponentInChildren<TempSlotUI>();
+    }
+
+    /// <summary>
+    /// 인벤토리 초기화용 함수
+    /// </summary>
+    /// <param name="playerInventory">이 UI가 표시할 인벤토리</param>
+    public void InitializeInventory(Inventory playerInventory)
+    {
+        inven = playerInventory; // 저장
+
+        for (uint i = 0; i < slotUIs.Length; i++)
+        {
+            slotUIs[i].InitializeSlot(inven[i]); // 모든 슬롯 초기화
+            slotUIs[i].onDragBegin += OnItemMoveBegin;
+            slotUIs[i].onDragEnd += OnItemMoveEnd;
+            slotUIs[i].onClick += OnSlotClick;
+        }
+
+        tempSLotUI.InitializeSlot(inven.TempSlot);
+    }
+
+    private void OnItemMoveBegin(uint index)
+    {
+        inven.MoveItem(index, tempSLotUI.Index); // 시작 -> 임시로 아이템 옮기기
+        tempSLotUI.Open(); // 임시 슬롯 열기
+    }
+
+    private void OnItemMoveEnd(uint index, bool isSlotEnd)
+    {
+        //uint finalIndex = index;
+        //if (!isSuccess)
+        //{
+        //    // 빈 곳을 찾아서 따로 준다.
+        //    if (inven.FindEmptySlot(out uint emptySlotIndex))
+        //    {
+        //        finalIndex = emptySlotIndex;
+        //    }
+        //    else
+        //    {
+        //        // 바닥에 드랍
+        //        Debug.LogWarning("바닥에 아이템을 드랍해야 한다.");
+        //        return;
+        //    }
+        //}
+
+        //inven.MoveItem(tempSLotUI.Index, finalIndex);
+        inven.MoveItem(tempSLotUI.Index, index); // 임시 -> 도착으로 아이템 옮기기
+
+        if (tempSLotUI.InvenSlot.IsEmpty)
+        {
+            tempSLotUI.Close(); // 임시 슬롯이 비면 닫는다.
+        }
+    }
+
+    private void OnSlotClick(uint index)
+    {
+        if (!tempSLotUI.InvenSlot.IsEmpty)
+        {
+            OnItemMoveEnd(index, true); // 슬롯이 클릭되었을 때 실행되기 때문에 isSlotEnd는 true
+        }
+    }
+}
