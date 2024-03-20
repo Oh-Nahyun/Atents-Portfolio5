62e25d2324748c6617b81b6ba306492321f26180
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-03-20 14:34:22 +0900

    Add files via upload

---
+++ 240320/Item/IconRotator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconRotator : MonoBehaviour
+{
+    public float rotateeSpeed = 360.0f;
+    public float moveSpeed = 2.0f;
+
+    public float minHeight = 0.5f;
+    public float maxHeight = 1.5f;
+
+    private void Start()
+    {
+        transform.Rotate(0, Random.Range(0, 360), 0); // 초기 랜덤 설정
+    }
+
+    private void Update()
+    {
+        // 위아래로 계속 움직임 (높이 : minHeight ~ maxHeight), 삼각함수 활용
+        // y축 기준으로 계속 회전
+    }
+}
