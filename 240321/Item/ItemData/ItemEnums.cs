69ffd0836435885fbc1ab37593daae44f091b11b
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-03-21 14:14:37 +0900

    Add files via upload

---
+++ 240321/Item/ItemData/ItemEnums.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 아이템 코드 (ID)
+/// </summary>
+public enum ItemCode
+{
+    Ruby = 0,
+    Emerald,
+    Sapphire,
+    CopperCoin,
+    SilverCoin,
+    GoldCoin
+}
+
+/// <summary>
+/// 아이템 정렬 기준
+/// </summary>
+public enum ItemSortBy
+{
+    Code = 0, // 코드 기준
+    Name, // 이름 기준
+    Price // 가격 기준
+}
