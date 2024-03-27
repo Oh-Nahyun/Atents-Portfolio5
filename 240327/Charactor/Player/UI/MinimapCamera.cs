c3dc1056781f92181846fa16a504ada828e324bd
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-03-27 14:22:42 +0900

    Add files via upload

---
+++ 240327/Charactor/Player/UI/MinimapCamera.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    /// 실습_240327
+    /// 플레이어를 따라다니는 느낌으로 작성하기
+}
---
+++ 240327/Item/Weapon.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    CapsuleCollider bladeCollider;
+    ParticleSystem ps;
+
+    private void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+        bladeCollider = GetComponent<CapsuleCollider>();
+    }
+
+    public void EffectEnable(bool enable)
+    {
+        if (enable)
+        {
+            ps.Play();
+        }
+        else
+        {
+            ps.Stop();
+        }
+    }
+
+    public void BladeColliderEnable(bool isEnable)
+    {
+        bladeCollider.enabled = isEnable;
+    }
+}
