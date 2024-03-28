ce55b4b19df3a614a14b26cc3f043e190bd017a8
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-03-28 14:10:16 +0900

    Add files via upload

---
+++ 240328/Charactor/Enemy/Waypoints.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 웨이포인트를 저장하고 처리하는 클래스
+/// </summary>
+public class Waypoints : MonoBehaviour
+{
+    public Transform Current => null;
+}
+
+/// 실습_240328
+/// 1. 적이 순찰 상태에 들어가면 일정 시간 대기한다.
+/// 2. 순찰 상태의 대기 시간이 다 되면 순찰상태로 변경되고, 다음 지점으로 이동한다.
+/// 3. 순잘 상태에서 목적지에 도착하면 다시 대기 상태로 들어간다.
